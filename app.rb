88216ab263798cc7960b4ce1dd0ae94316e81eae
Artem134134 <car322132@gmail.com>
2024-01-08 22:57:40 +0300

    Add post method in file app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
  get '/'  do 
- 	  erb :index
+ 	erb :index
  end
+
+ post '/' do 
+ 	@user_name = params[:user_name]
+ 	@phone     = params[:phone]
+ 	@date_time = params[:date_time]
+
+ 	@title = 'Thankee!'
+ 	@message = "Dear #{@user_name}, we will be waiting at #{@date_time} in our Barber Shope!"
+
+ 	erb :flash_messages
+ end	
+
+ 
