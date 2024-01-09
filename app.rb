da25bfb9e980c48d5ab42ad5493134fbcc8162f5
Artem134134 <car322132@gmail.com>
2024-01-09 23:22:36 +0300

    Admin zone added.

--- app.rb
+++ app.rb
@@ -9,13 +9,52 @@ require 'sinatra'
  	@phone     = params[:phone]
  	@date_time = params[:date_time]
 
- 	@title = 'Thankee!'
- 	@message = "Dear #{@user_name}, we will be waiting at #{@date_time} in our Barber Shope!"
+    @name_page = 'Main page'
 
- 	file = File.open 'visitors.txt', 'a'
- 	file.write "User: #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time} \n"
- 	file.close
- 	erb :flash_messages
+ 	if @user_name == '' && @phone == '' && @date_time == ''
+ 	    @title = 'Not accepted!'
+ 	    @message = 'Error, fill in all fields!  :-('
+ 	    
+ 	    erb :flash_messages
+ 	else 
+	 	@title = 'Thankee!  :-)'
+	 	@message = "Dear - #{@user_name}, we will be waiting at #{@date_time} in our Barber Shope!"
+    
+	 	file = File.open 'visitors.txt', 'a'
+	 	file.write "User - #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time} \n"
+	 	file.close
+	 	erb :flash_messages
+	end	
+ end
+
+ get '/admin' do 
+	
+ 	@name_page = 'Main page'
+
+ 	erb :admin
  end	
 
- 
+post '/admin' do 
+
+	@login = params[:login]
+	@password = params[:password]
+
+	@name_page = 'Main page'
+
+	if @login == 'Art' and @password == 'Volga'
+
+		@title = 'Admin Zone'
+ 		@message = 'Hi admin - Art@Volga'
+
+ 		@logfile = File.open("visitors.txt")
+
+        erb :flash_messages
+    else
+          
+       @message = "Access is denied!"
+
+          erb :admin
+       end
+
+
+end
